2b090fad00b6499e1afa6ec314116be11fe1a1f7
Asilbek Muminov <asilbekmuminov@Asilbeks-MacBook-Air.local>
2024-04-28 19:02:25 +0200

    fix: api keys were exposed, now fixed with env;

--- src/firebase.js
+++ src/firebase.js
@@ -6,15 +6,13 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = process.env.REACT_APP_FIREBASE_CONFIG;
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCIQSnluJdFihd4E23CaetW0h_GPcUiB24",
-  authDomain: "rhapsody-749ee.firebaseapp.com",
-  projectId: "rhapsody-749ee",
-  storageBucket: "rhapsody-749ee.appspot.com",
-  messagingSenderId: "121567102251",
-  appId: "1:121567102251:web:712cdb114db2c6ec3d316e"
+  apiKey: `${process.env.REACT_APP_FIREBASE_API}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  projectId:  `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId:  `${process.env.REACT_APP_FIREBASE_APP_ID}`,
 };
 
 // Initialize Firebase
