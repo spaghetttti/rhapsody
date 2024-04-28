01bb5768ccba5f2bfc92ff09a805b6b5562a03cf
Asilbek Muminov <asilbekmuminov@Asilbeks-MacBook-Air.local>
2024-04-28 20:07:30 +0200

    fix: .env file fix

--- src/firebase.js
+++ src/firebase.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
   appId:  `${process.env.REACT_APP_FIREBASE_APP_ID}`,
 };
 
+console.log(firebaseConfig)
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
